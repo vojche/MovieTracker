bb9de671bb15f9aa90109f95e38d89134e3f9b7d
Vojche Stojanoski <vstojanoski@insynctive.com>
2016-05-04 01:32:59 +0200

    EntityDB

---
+++ MovieTracker/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker
+{
+    class Movie
+    {
+        public int id { get; set; }
+        public int imdbID { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public int runtime { get; set; }
+        public List<string> genres { get; set; }
+        public string director { get; set; }
+        public List<string> actors { get; set; }
+        public string plot { get; set; }
+        public string awards { get; set; }
+        public string image { get; set;}
+        public double rating { get; set; }
+    }
+}
